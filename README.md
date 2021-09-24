4266c54e74be64a045b730bb59d527600355173b
Rushil Chawla <rushilchawla12@gmail.com>
2021-09-24 16:57:10 +0800

    3 commit- editing read me file and adding comments to js file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Week-3-Homework---Password-Generator
+
+Link to github repo: https://github.com/rushilchawla/Password-Generator
+Link to active page: https://rushilchawla.github.io/Password-Generator/
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Defines the constants by selecting them in the document
 const characterAmountRange = document.getElementById('characterAmountRange')
 const characterAmountNumber = document.getElementById('characterAmountNumber')
 const includeUppercaseElement = document.getElementById('includeUppercase')
@@ -6,9 +7,12 @@ const includeSymbolsElement = document.getElementById('includeSymbols')
 const form = document.getElementById('passwordGeneratorForm')
 const passwordDisplay = document.getElementById('passwordDisplay')
 
+//Selects the characters for the ASCII character codes 
+//Low to high, meaning starting the lower number (65 (A)) to high number (90(Z))
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
 const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
 const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+//Concat helps add more arrays
 const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
   arrayFromLowToHigh(58, 64)
 ).concat(
@@ -20,6 +24,10 @@ const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
 characterAmountNumber.addEventListener('input', syncCharacterAmount)
 characterAmountRange.addEventListener('input', syncCharacterAmount)
 
+
+//Event listener listens to the submit event for the the form
+//preventDefault helps not refreshing the page with the form is submitted
+//generate password includes all options for the password if the boxes are 'checked'
 form.addEventListener('submit', e => {
   e.preventDefault()
   const characterAmount = characterAmountNumber.value
@@ -30,12 +38,15 @@ form.addEventListener('submit', e => {
   passwordDisplay.innerText = password
 })
 
+//By default, prints a lowercase password
 function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
   let charCodes = LOWERCASE_CHAR_CODES
   if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
   if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
   if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
   
+  //Math.random chooses a random number between 0 and our character number
+  //Math.floor makes sure that this number is an integer
   const passwordCharacters = []
   for (let i = 0; i < characterAmount; i++) {
     const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
@@ -44,6 +55,7 @@ function generatePassword(characterAmount, includeUppercase, includeNumbers, inc
   return passwordCharacters.join('')
 }
 
+//generates an empty array for our low character code to a high character code
 function arrayFromLowToHigh(low, high) {
   const array = []
   for (let i = low; i <= high; i++) {
@@ -52,6 +64,8 @@ function arrayFromLowToHigh(low, high) {
   return array
 }
 
+
+//everytime I change the range input, the number also changes next to it
 function syncCharacterAmount(e) {
   const value = e.target.value
   characterAmountNumber.value = value
